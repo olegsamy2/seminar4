04564022c81fa7472390298a9cdd153a2c9a5863
OlegSam2 <doorsmania@mail.ru>
2022-08-17 20:10:13 +0300

    quantity of digits in a number

---
+++ task26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// Не используем в решении задачи строки и ее свойства
+
+Console.WriteLine("введите число А");
+int number = Convert.ToInt32(Console.ReadLine());
+int count;
+//int sum = 0;
+for (count=0; number > 0; count++)
+{
+    number /= 10;
+    
+}
+Console.WriteLine (count); 
